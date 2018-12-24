e7133240e2dae330bc95b838fc4877e0aa21e9ab
exoego <ytatsuno.jp@gmail.com>
2018-12-24 10:03:19 +0900

    Conver seeAlso tag to (at)see annotation

--- src/main/scala/com/leeriggins/awsapis/gen/ScalaJsGen.scala
+++ src/main/scala/com/leeriggins/awsapis/gen/ScalaJsGen.scala
@@ -140,6 +140,7 @@ class ScalaJsGen(
        |  }""".stripMargin
   }
 
+  private final val seeAlso = """<div class="seeAlso">\s*<a href="([^"]+)">([^<]+)</a>\s*</div>""".r
   private final val fieldReference = "<a>(\\w+)\\$(\\w+)</a>".r
   private final val listItemPattern = "<li>\\s*(.*?)\\s*</li>".r
   private final val paragraphPattern = "<p>\\s*(.*?)\\s*</p>".r
@@ -157,6 +158,7 @@ class ScalaJsGen(
         }),
         doc => doc.replaceAllLiterally("$", ""),
         doc => notePattern.replaceAllIn(doc, "\n'''Note:'''"),
+        doc => seeAlso.replaceAllIn(doc, matched => s"\n@see See also [[${matched.group(1)}|${matched.group(2)}]]"),
         doc => removeTagPattern.replaceAllIn(doc, _ => s""),
         doc => listItemPattern.replaceAllIn(doc, matched => s"* ${matched.group(1)}\n"),
         doc => paragraphPattern.replaceAllIn(doc, matched => s"${matched.group(1)}\n"),
