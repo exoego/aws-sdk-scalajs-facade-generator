58335a34d06fc87d0ed0f34c1804b4fa0b0e4baa
exoego <ytatsuno.jp@gmail.com>
2020-06-26 22:05:05 +0900

    Avoid unintentional eta-extension for Scala 3 compatibility

--- src/main/scala/com/leeriggins/awsapis/gen/ScalaJsGen.scala
+++ src/main/scala/com/leeriggins/awsapis/gen/ScalaJsGen.scala
@@ -109,7 +109,7 @@ class ScalaJsGen(projectDir: File, api: Api) {
         operation.deprecated match {
           case Some(true) => ""
           case _ =>
-            s"  @inline def ${methodName}Future(${parameters}): Future[${outputType}] = service.${methodName}(${arg}).promise.toFuture"
+            s"  @inline def ${methodName}Future(${parameters}): Future[${outputType}] = service.${methodName}(${arg}).promise().toFuture"
         }
     }
 
